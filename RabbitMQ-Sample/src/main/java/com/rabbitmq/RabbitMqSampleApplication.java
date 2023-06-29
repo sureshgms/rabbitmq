3dc9c80de5728ca7bbccd3f7207d6e734553ee0b
Suresh <suresh.kenchegowda@circles.asia>
2023-06-29 17:23:36 +0530

    Initial Commit

---
+++ RabbitMQ-Sample/src/main/java/com/rabbitmq/RabbitMqSampleApplication.java
@@ -0,0 +1,13 @@
+package com.rabbitmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitMqSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitMqSampleApplication.class, args);
+	}
+
+}
